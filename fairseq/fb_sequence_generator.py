9c0332f9b14ee26b1ce06f0cefbd9056ad448fcc
Edan Tessel Sneh <edan@fb.com>
2021-06-29 15:09:13 -0700

    Back out "Adding FBSequenceGenerator"
    Summary:
    Original commit changeset: b7a83bbc719d
    
    Reverts commit D26228721 (https://github.com/fairinternal/fairseq-py/commit/ecbd7dbf9c0e44f084d8b6db54d43146c17ecf73)
    
    Reviewed By: theweiho
    
    Differential Revision: D29369494
    
    fbshipit-source-id: 9e745b11bc532ca8ced2816326aa94afbb46ba2d

--- fairseq/fb_sequence_generator.py
+++ fairseq/fb_sequence_generator.py
@@ -8,7 +8,6 @@ from typing import Dict, List, Optional
 
 import torch
 from fairseq.sequence_generator import EnsembleModel
-from fairseq.sequence_generator import SequenceGenerator
 from torch import Tensor
 
 
@@ -104,7 +103,3 @@ class FBEnsembleModelWithFork(EnsembleModel):
             model.decoder.reorder_incremental_state_scripting(
                 incremental_states[i], new_order
             )
-
-
-class FBSequenceGenerator(SequenceGenerator):
-    pass
